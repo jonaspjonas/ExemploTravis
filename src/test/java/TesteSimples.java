bf853f477b5cf37d38a767c2d5f8797006c96382
jonaspjonas <jonaspjonas@gmail.com>
2021-04-04 13:38:12 -0300

    "Testando travis02"

--- src/test/java/TesteSimples.java
+++ src/test/java/TesteSimples.java
@@ -6,4 +6,10 @@ public class TesteSimples {
     public void teste1(){
         System.out.println("teste 01 funcionando");
     }
+
+    @Test
+    public void teste2(){
+        System.out.println("teste 02 funcionando");
+    }
 }
+
